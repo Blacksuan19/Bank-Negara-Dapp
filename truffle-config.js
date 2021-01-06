daf614adbd1ac4043d30af7886e0eac343a16d56
blacksuan19 <abubakaryagob@gmail.com>
2021-01-06 17:18:13 +0800

    config: depoly to ropstan test network
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,23 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic =
+  "message ostrich vanish ten surround palm window huge mule dance claim matrix";
+// deployed contract https://ropsten.etherscan.io/address/0x8f129e60fd0a99128b234ab2ad434178df78442d
+
 module.export = {
   networks: {
     development: {
       host: "localhost",
-      port: 7545,
-      network_id: "*",
+      port: 7545, // ganache cli
+      network_id: 5777,
       gas: 5000000,
+    },
+    ropsten: {
+      provider: function () {
+        const URI =
+          "https://ropsten.infura.io/v3/e872c65194d141d7a9ee7566aa6b9387";
+        return new HDWalletProvider(mnemonic, URI);
+      },
+      network_id: 3,
     },
   },
   compilers: {
